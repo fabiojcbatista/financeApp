c849b2c87e4d9eedfcbf46a74ffe5b600c8fa8d3
Fabio Jr <fabiojcbweb@gmail.com>
2023-02-21 18:36:11 -0300

    Feat: Inclui movimentações

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,63 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header";
 import Balance from "../../components/Balance";
+import Movements from "../../components/Movements";
 
-export default function Home(){
-  return (
-    <View style={styles.container}>
-      <Header name="Fabio Jr"/>
-      <Balance saldo="9.000.00" gastos="-500.00"/>
-    </View>
-  );
+const list = [{
+    id: 1,
+    label: 'Boleto conta luz',
+    value: '250,00',
+    date: '10/02/2023',
+    type: 0 //despesas
+},
+{
+    id: 2,
+    label: 'Pix cliente X',
+    value: '500,00',
+    date: '10/02/2023',
+    type: 1 //receitaS/entradas
+},
+{
+    id: 3,
+    label: 'Salário',
+    value: '2000,00',
+    date: '01/02/2023',
+    type: 1 //receitas/entrada
+}];
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Header name="Fabio Jr" />
+            <Balance saldo="9.000.00" gastos="-500.00" />
+            <Text style={styles.title}>Últimas movimentações</Text>
+            <FlatList 
+            style={styles.list}
+            data={list}
+            keyExtractor={(item)=> String(item.id)}
+            showsVerticalScrollIndicator={false}
+            renderItem={({item})=><Movements data={item}/>}
+            />
+        </View>
+    );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fafafa',
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        margin: 14
+
+    },
+    list:{
+        fontWeight: 'bold',
+        marginLeft: 14,
+        marginRight: 14,
+        marginTop: 14,
+    }
 });
